db4667178c2a2d5089a8db7c3ef912eb0a3f78ae
Keith Schacht <krschacht@gmail.com>
2010-09-11 14:45:41 -0700

    seed migration with some initial data

--- lib/rails/generators/moxie_forum/templates/migration.rb
+++ lib/rails/generators/moxie_forum/templates/migration.rb
@@ -1,6 +1,15 @@
 class CreateMoxieForumTables < ActiveRecord::Migration
   def self.up
     INSERT_SCHEMA
+    
+    # Seed with some initial data
+    
+    f = Moxie::Forum.create( :title => "Sample forum", :description => "Description of this forum" )
+    t1 = Moxie::Topic.create( :forum => f, :title => "First topic" )
+    t2 = Moxie::Topic.create( :forum => f, :title => "Second topic" )
+    Moxie::Post.create( :topic => t1, :body => "First post in the first topic" )
+    Moxie::Post.create( :topic => t1, :body => "Second post in the first topic" )
+    Moxie::Post.create( :topic => t2, :body => "First post in the second topic" )
   end
 
   def self.down
